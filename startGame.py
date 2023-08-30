0e653d727915ee90ad7b5f8d94ab8717899794d6
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2023-08-30 16:44:29 -0300

    Update startGame.py

--- startGame.py
+++ startGame.py
@@ -1,35 +1,20 @@
-import sys
-from threading import Thread
+import socket
+import game
+import server
+import client
+import tkinter as tk
+from tkinter import messagebox
 
-from PySide2 import QtWidgets
+def start():
+    try:
+        # Tentar iniciar como cliente
+        client.start_client()
+    except socket.error:
+        # Se falhar, iniciar como servidor
+        try:
+            server.start_server()
+        except Exception as e:
+            messagebox.showerror("Erro", f"Erro ao iniciar o servidor: {e}")
 
-from game import JogoDaVelha3D_GUI
-from client import Client
-from server import Server
-
-class UserName(QtWidgets.QWidget):
-    # ... (manter a estrutura original deste widget) ...
-
-    def enterGame(self):
-        self.ui = UI(self.userNameBox.text())
-        self.client = Client(self.ui, self.serverIpBox.text(), self.serverPortBox.text())
-        self.close()
-
-    def hostGame(self):
-        t1 = Thread(target=self.initServer)
-        t1.start()
-        t2 = Thread(target=self.initGame)
-        t2.start()
-
-    def initServer(self):
-        self.server = Server(self.serverPortBox.text())
-
-    def initGame(self):
-        self.ui = UI(self.userNameBox.text())
-        self.client = Client(self.ui, self.serverIpBox.text(), self.serverPortBox.text())
-        self.close()
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    UserName().show()
-    sys.exit(app.exec_())
+if __name__ == '__main__':
+    start()
