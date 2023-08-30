0ebf10655c23cbf43ab98d5174a3717cf8f9d206
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2023-08-30 16:43:40 -0300

    Updating client.py

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import socket
 
 class Client:
-    def __init__(self, ui, server, port):
-        self.ui = ui
+    def __init__(self, server, port):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = server
         self.port = port
@@ -13,7 +12,8 @@ class Client:
         while True:
             message = self.client.recv(2048).decode('utf-8')
             if len(str(message)) != 0:
+                print(message)
                 self.ui.onMessageReceived(message)
 
     def sendMessage(self, msg):
-        self.client.sendall(msg.encode('utf-8'))
+        self.client.sendall(msg)
